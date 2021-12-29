fd046be6000f7b70b434ec3db454bee343f26beb
Tyler Woodley <55413500+sixeight120bpm@users.noreply.github.com>
2021-12-29 09:57:43 -0800

    Add GoogleMaps.bundle to project
    Project builds and map is shown!

--- ClinicalInc_MobileTechAssessment/ClinicalInc_MobileTechAssessment/ViewController.m
+++ ClinicalInc_MobileTechAssessment/ClinicalInc_MobileTechAssessment/ViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "ViewController.h"
+#import <GoogleMaps/GoogleMaps.h>
 
 @interface ViewController ()
 
@@ -15,7 +16,22 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+  // Do any additional setup after loading the view.
+  // Create a GMSCameraPosition that tells the map to display the
+  // coordinate -33.86,151.20 at zoom level 6.
+  GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
+                                                          longitude:151.20
+                                                               zoom:6];
+  GMSMapView *mapView = [GMSMapView mapWithFrame:self.view.frame camera:camera];
+  mapView.myLocationEnabled = YES;
+  [self.view addSubview:mapView];
+
+  // Creates a marker in the center of the map.
+  GMSMarker *marker = [[GMSMarker alloc] init];
+  marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+  marker.title = @"Sydney";
+  marker.snippet = @"Australia";
+  marker.map = mapView;
 }
 
 
