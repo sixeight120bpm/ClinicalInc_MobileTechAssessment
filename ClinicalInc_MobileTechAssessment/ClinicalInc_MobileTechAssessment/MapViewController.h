c73fcefff6d12daa5227a29b978220564cc018dc
Tyler Woodley <55413500+sixeight120bpm@users.noreply.github.com>
2022-01-15 14:33:50 -0800

    Extract map controler code to new view controller

---
+++ ClinicalInc_MobileTechAssessment/ClinicalInc_MobileTechAssessment/MapViewController.h
@@ -0,0 +1,14 @@
+//
+//  MapViewController.h
+//  ClinicalInc_MobileTechAssessment
+//
+//  Created by user206074 on 1/15/22.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface MapViewController : UIViewController <CLLocationManagerDelegate>
+
+
+@end
